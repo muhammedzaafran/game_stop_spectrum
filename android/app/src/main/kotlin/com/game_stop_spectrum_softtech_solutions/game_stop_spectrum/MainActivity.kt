e546e0ecde0e0344e46831591a14ca66bfc18bb3
muhammedzaafran <zaafranmuhammed@gmal.com>
2024-01-04 10:20:27 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/game_stop_spectrum_softtech_solutions/game_stop_spectrum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.game_stop_spectrum_softtech_solutions.game_stop_spectrum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:game_stop_spectrum/view/auth_ui/splash_screen.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return const ScreenUtilInit(
+      designSize: Size(428, 926),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: GetMaterialApp(
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
