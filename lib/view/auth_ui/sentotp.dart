d3912a723c5ec58ba54ef8137e2dc7b187b83585
muhammedzaafran <zaafranmuhammed@gmal.com>
2024-01-29 17:45:52 +0530

    done.

--- lib/view/auth_ui/sentotp.dart
+++ lib/view/auth_ui/sentotp.dart
@@ -33,68 +33,70 @@ class _SendOtpPnoState extends State<SendOtpPno> {
           backgroundColor: AppConstant.transparent,
           elevation: 0,
         ),
-        body: Container(
-            alignment: Alignment.center,
-            width: ScreenUtil().screenWidth,
-            height: ScreenUtil().screenHeight,
-            decoration: const BoxDecoration(color: AppConstant.black),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 150.h,
-                ),
-                Lottie.asset('asset/json/playstation.json',
-                    width: 100.w,
-                    height: 100.h,
-                    animate: true,
-                    fit: BoxFit.cover,
-                    repeat: true),
-                Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: Get.width * 0.85.w,
-                          height: 60.h,
-                          child: CustomTextField(
-                              keyboardType: TextInputType.number,
-                              validateInput: (value) =>
-                                  Validator.validatePhoneNumber(
-                                      phoneNumber: value),
-                              controller: _sendTextController,
-                              prefixIcon: const Icon(
-                                Icons.phone_android_sharp,
-                                color: Colors.black,
-                              ),
-                              hintText: "      enter number",
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 15.0.h, horizontal: 12.0.w)),
-                        ),
-                        SizedBox(
-                          child: CustomElevatedBtn(
-                            height: 30.h,
-                            backgroundColor: AppConstant.appBtnColor,
-                            foregroundColor: AppConstant.appMainColor,
-                            title: "GET OTP",
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                String phoneNumber =
-                                    "+91" + _sendTextController.text.trim();
-                                sentOtpController.sendOtp(phoneNumber);
-                              }
-                            },
-                            textColor: Colors.white,
-                            width: Get.width * 0.30.w,
+        body: SingleChildScrollView(
+          child: Container(
+              alignment: Alignment.center,
+              width: ScreenUtil().screenWidth,
+              height: ScreenUtil().screenHeight,
+              decoration: const BoxDecoration(color: AppConstant.black),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 150.h,
+                  ),
+                  Lottie.asset('asset/json/playstation.json',
+                      width: 100.w,
+                      height: 100.h,
+                      animate: true,
+                      fit: BoxFit.cover,
+                      repeat: true),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: Get.width * 0.85.w,
+                            height: 60.h,
+                            child: CustomTextField(
+                                keyboardType: TextInputType.number,
+                                validateInput: (value) =>
+                                    Validator.validatePhoneNumber(
+                                        phoneNumber: value),
+                                controller: _sendTextController,
+                                prefixIcon: const Icon(
+                                  Icons.phone_android_sharp,
+                                  color: Colors.black,
+                                ),
+                                hintText: "      enter number",
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 15.0.h, horizontal: 12.0.w)),
                           ),
-                        ),
-                      ],
-                    ))
-              ],
-            )),
+                          SizedBox(
+                            child: CustomElevatedBtn(
+                              height: 30.h,
+                              backgroundColor: AppConstant.appBtnColor,
+                              foregroundColor: AppConstant.appMainColor,
+                              title: "GET OTP",
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  String phoneNumber =
+                                      "+91" + _sendTextController.text.trim();
+                                  sentOtpController.sendOtp(phoneNumber);
+                                }
+                              },
+                              textColor: Colors.white,
+                              width: Get.width * 0.30.w,
+                            ),
+                          ),
+                        ],
+                      ))
+                ],
+              )),
+        ),
       ),
     );
   }
